57948eae1de5ac670233f555670008201b0f75a1
lindenkron <lindenkron@hotmail.com>
2019-02-11 14:41:33 +0100

    Add files via upload

--- js/event.js
+++ js/event.js
@@ -1,25 +1,3 @@
-var field_data = {
-    title: {
-        fn: "Title",
-        ht: "Discord ID including discriminator",
-        al: "title"
-    },
-    desc: {
-        fn: "Description",
-        ht: "Event description",
-        al: "desc"
-    },
-    date: {
-        fn: "Date",
-        ht: "Event date",
-        al: "date"
-    },
-    dura: {
-        fn: "Duration",
-        ht: "Event duration",
-        al: "dura"
-    }
-};
 
 var mm = {
     dark: {
@@ -32,31 +10,61 @@ var mm = {
     }
 };
 
+
 function updateSyntax() {
     var title = $('#title-field').val();
+    var other = $('#other-field').val();
+    var titlefield = '';
     var desc = $('#desc-field').val();
+    var time = $('#time-field').val();
     var date = $('#date-field').val();
     var dura = $('#dura-field').val();
-    var bugtext = '';
-    if (title && desc && date && dura) {
-        bugtext = '-event "' + title + '" "' + desc + '" "' + date + '" "' + dura + '"';
+    var apptext = '';
+
+    if (title != 'Other') { titlefield = title;} else { titlefield = other; }
+
+    var stringdate = new Date(date);
+    var options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    var newdate = stringdate.toLocaleString('en-GB', options);
+
+    if (titlefield && desc && time && (newdate != 'Invalid Date') && dura) {
+        apptext = '-event "' + titlefield + '" "' + desc + '" "' + time + '" "' + newdate + '" "' + dura + '"';
     }
-    $('#syntax').text(bugtext);
-    $('#lrg-rep').toggleClass('hidden', bugtext.length < 800);
+    $('#syntax').text(apptext);
+    $('#lrg-rep').toggleClass('hidden', apptext.length <= 1000);
 }
 
-function updateField(event) {
+function pageLoad(page) {
     window.sct = 1;
-    $('#add-btn').off('click');
-    $('#del-btn').off('click');
-    switch(event.target.value) {
-        default:
-            if (event.target.value in field_data) {
-                var field_html = '<label for="' + event.target.value + '-field">' + field_data[event.target.value].fn + '</label><p class="help-text" id="' + event.target.value + '-help">' + field_data[event.target.value].ht + '</p><input type="text" id="' + event.target.value + '-field" aria-describedby="' + event.target.value + '-help" required>';
-            }
+    var cb_btn = '';
+    var st = '';
+    switch (page) {
+        case "create":
+            $('div#content').on('input', 'input[id*="-field"]', updateSyntax);
+            $('div#content').on('input', 'select[id*="-field"]',updateSyntax);
+            $('div#content').on('input', 'textarea[id*="-field"]',updateSyntax);
+            cb_btn = '#copy-btn';
+            st = '#syntax';
+            break;
+    }
+    var cb = new ClipboardJS(cb_btn, {
+        text: function(trigger) {
+            return $(st).text();
+        }
+    });
+    cb.on('success', function(e) {
+        $(e.trigger).html('Copied');
+        setTimeout(function() {
+        $(e.trigger).html('Copy');
+        }, 2000);
+    });
+    $('body').on('click', 'a[id*="switch-"]', switchMode);
+    if (loadTheme()) {
+        switchMode();
     }
 }
 
+// Anything under here don't mess with, it's themes
 function loadTheme() {
     var light = false;
     if (typeof(Storage) !== 'undefined') {
@@ -83,33 +91,4 @@ function switchMode() {
     $('#switch-mobile').html('<i class="far fa-' + mm[bc].m + '"></i>');
     $('#switch-desktop').html('<i class="far fa-' + mm[bc].m + '"></i> ' + mm[bc].d);
     setTheme();
-}
-
-function pageLoad(page) {
-    window.sct = 1;
-    var cb_btn = '';
-    var st = '';
-    switch (page) {
-        case "create":
-            $('div#content').on('input', 'input[id*="-field"]', updateSyntax);
-            $('div#content').on('input', 'textarea[id*="-field"]',updateSyntax);
-            cb_btn = '#copy-btn';
-            st = '#syntax';
-            break;
-    }
-    var cb = new ClipboardJS(cb_btn, {
-        text: function(trigger) {
-            return $(st).text();
-        }
-    });
-    cb.on('success', function(e) {
-        $(e.trigger).html('Copied');
-        setTimeout(function() {
-        $(e.trigger).html('Copy');
-        }, 2000);
-    });
-    $('body').on('click', 'a[id*="switch-"]', switchMode);
-    if (loadTheme()) {
-        switchMode();
-    }
 }
